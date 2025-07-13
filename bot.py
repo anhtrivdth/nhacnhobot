e1e8640a7d954339736db11ea18df8f787788b94
anhtrivdth <130107736+anhtrivdth@users.noreply.github.com>
2025-07-13 22:43:33 +0700

    Update bot.py
    A

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 import os
 
-BOT_TOKEN = os.environ.get("7806552255:AAF0grPJ2p6a9haqzFgNacRNrXt-1ZipXv4")  # hoặc thay bằng chuỗi token trực tiếp
+BOT_TOKEN = os.environ.get("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ Bot đang hoạt động!")
